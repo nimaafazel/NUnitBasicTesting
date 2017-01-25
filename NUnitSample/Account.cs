1816989c5ec560c2cfa221502c004c96051b2d83
Nima Afazel <c147780@05505337DT.HOSTED.LAC.COM>
2017-01-25 09:36:02 -0800

    First commit

---
+++ NUnitSample/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NUnitSample
+{
+    public class Account
+    {
+        private decimal balance;
+        private decimal minimumBalance = 10m;
+
+        public void Deposit(decimal amount)
+        {
+            balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            balance -= amount;
+        }
+
+        public void TransferFunds(Account destination, decimal amount)
+        {
+            if (balance - amount < minimumBalance)
+                throw (new InsufficientFundsException());
+
+            destination.Deposit(amount);
+            
+            Withdraw(amount);
+        }
+
+        public decimal Balance
+        {
+            get { return balance; }
+        }
+
+        public decimal MinimumBalance
+        {
+            get { return minimumBalance; }
+        }
+    }
+
+    /// <summary>
+    /// Custom class for a custom exception.
+    /// </summary>
+    public class InsufficientFundsException : ApplicationException
+    {
+
+    }
+}
---
+++ NUnitSample/AccountTest.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace NUnitSample
+{
+    [TestFixture]  // indicates that this is an NUnit test class
+    public class AccountTest
+    {
+        Account source;
+        Account destination;
+
+        [SetUp] // indicates that this method will be applied first to any test method.
+        public void Init()
+        {
+            source = new Account();
+            source.Deposit(200m);
+
+            destination = new Account();
+            destination.Deposit(150m);
+        }
+
+        [Test] // indicates that this is a NUnit test method and it will be run with the NUnit runner.
+        public void TransferFunds()
+        {
+            /*
+             * This initialization is now on SetUp
+            Account source = new Account();
+            source.Deposit(200m);
+
+            Account destination = new Account();
+            destination.Deposit(150m);
+            */
+
+            source.TransferFunds(destination, 100m);
+
+            Assert.AreEqual(250m, destination.Balance);
+            Assert.AreEqual(100m, source.Balance);
+        }
+
+        [Test]        
+        [Ignore("Decide how to implement transaction management")] // indicates that this method will not be run by the test runner.
+        public void TransferWithInsufficientFundsAtomicity()
+        {
+            try
+            {
+                source.TransferFunds(destination, 300m);
+            }
+            catch (InsufficientFundsException expected)
+            {
+                Console.Write(expected.Message);
+            }
+
+            Assert.AreEqual(200m, source.Balance);
+            Assert.AreEqual(150m, destination.Balance);            
+        }
+    }
+}
